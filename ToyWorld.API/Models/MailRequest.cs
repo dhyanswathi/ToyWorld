52ab0a0de31551b454170a66592af2d6b246d241
swathi <swathysidharthan@gmail.com>
2023-05-05 09:30:57 +0200

    update readme

--- ToyWorld.API/Models/MailRequest.cs
+++ ToyWorld.API/Models/MailRequest.cs
@@ -2,9 +2,9 @@
 {
     public class MailRequest
     {
-        public string ToEmail { get; set; }
-        public string Subject { get; set; }
-        public string Body { get; set; }
+        public string? ToEmail { get; set; }
+        public string? Subject { get; set; }
+        public string? Body { get; set; }
         //public List<IFormFile> Attachments { get; set; }
     }
 }
