c728341807118ac975f525df0637ff86ee6aadd4
swathi <swathysidharthan@gmail.com>
2023-05-11 12:11:32 +0200

    add user controller

---
+++ ToyWorld.API/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ToyWorld.API.DTO;
+using ToyWorld.API.Models;
+
+namespace ToyWorld.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _repo;
+        public UsersController(IUserRepository repo) 
+        { 
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IActionResult GetUser(Guid id)
+        {
+            try
+            {
+                var user = _repo.GetUserById(id);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.ToString());
+            }
+        }
+
+        [HttpPost("register")]
+        public IActionResult PostUser(UserRegister user)
+        {
+            var result = _repo.Register(user);
+            return Created("", result);
+        }
+    }
+}
