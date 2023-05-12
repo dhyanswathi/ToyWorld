03179bf2a90c6871d66f2a5d54fffe7d1312e9dd
swathi <swathysidharthan@gmail.com>
2023-05-12 20:02:54 +0200

    add login authentication in userscontroller

--- ToyWorld.API/DTO/LoginResponse.cs
+++ ToyWorld.API/DTO/LoginResponse.cs
@@ -2,7 +2,7 @@
 {
     public class LoginResponse
     {
-        public string Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Token { get; set; }
