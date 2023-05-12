0977e0868146aae7d53ba3d8fad1a72c4727f6d6
swathi <swathysidharthan@gmail.com>
2023-05-12 19:45:54 +0200

    add dto loginresponse

---
+++ ToyWorld.API/DTO/LoginResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace ToyWorld.API.DTO
+{
+    public class LoginResponse
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+
+    }
+}
